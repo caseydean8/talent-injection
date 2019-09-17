b07876029a3e80804e3c26b49f6831fdd751d893
Casey Carroll <caseydean8@gmail.com>
2019-09-17 11:07:04 -0400

    projects added, all links functional

--- javascript/app.js
+++ javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(event){
     // event.preventDefault();
     $(".projects").click(function(){
         console.log("button clicked");
-        $("#text-content").text("click image for deployed project").fadeIn();
+        $("#text-content").text("click image for deployed project").fadeIn(1000);
       $('.project').fadeIn(800);
     //   var appBtn = $("<button>");
     //   var gitBtn = $("<button>");
