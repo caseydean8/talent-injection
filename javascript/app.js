a35c51c353c20b9095b9debbd76a5ee3ba050aa3
Casey Carroll <caseydean8@gmail.com>
2019-09-16 21:04:08 -0400

    app and github buttons added.

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,16 @@
 $(document).ready(function(event){
     // event.preventDefault();
     $(".projects").click(function(){
-        console.log("button clicked")
-      $('.two').show();
+        console.log("button clicked");
+      $('.project').show();
+      var appBtn = $("<button>");
+      var gitBtn = $("<button>");
+      appBtn.attr({ id: "app-button" });
+      appBtn.text("app");
+      gitBtn.attr({ id: "github"});
+      gitBtn.text("github");
+      $(".project").append("<br>");
+      $(".project").append(appBtn);
+      $(".project").append(gitBtn);
     });
 });
