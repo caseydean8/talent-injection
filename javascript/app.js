df90c5b491ea6681aca35c35ed309ac3c40b00f7
Casey Carroll <caseydean8@gmail.com>
2019-09-16 20:34:36 -0400

    Shows images on button click.

---
+++ javascript/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(event){
+    // event.preventDefault();
+    $(".projects").click(function(){
+        console.log("button clicked")
+      $('.two').show();
+    });
+});
