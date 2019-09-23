ae9a0dcffe40324f13d856362538567083071a27
Casey Carroll <caseydean8@gmail.com>
2019-09-23 10:24:51 -0400

    Changed fonts to adobe garamond and nunito.

--- javascript/app.js
+++ javascript/app.js
@@ -2,17 +2,8 @@ $(document).ready(function(event){
     // event.preventDefault();
     $(".project-btn").click(function(){
         console.log("button clicked");
-        $("#text-content").text("click image for deployed project").fadeIn(1000);
+        $("#text-content").text("tap image for deployed project").fadeIn(1000);
       $('.project').fadeIn(800);
       $('#contact').fadeIn(1000);
-    //   var appBtn = $("<button>");
-    //   var gitBtn = $("<button>");
-    //   appBtn.attr({ id: "app-button" });
-    //   appBtn.text("app");
-    //   gitBtn.attr({ id: "github"});
-    //   gitBtn.text("github");
-    //   $(".project").append("<br>");
-    //   $(".project").append(appBtn);
-    //   $(".project").append(gitBtn);
     });
 });
