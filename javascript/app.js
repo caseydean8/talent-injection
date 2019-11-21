8d9f91c535ba8fbf82476d01bce40f60dc5a4709
Casey Carroll <caseydean8@gmail.com>
2019-11-21 10:11:10 -0500

    changed button fade

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function(event){
     // event.preventDefault();
     $(".project-btn").click(function(){
-        console.log("button clicked");
         $("#text-content").text("tap image for deployed project").fadeIn(1000);
       $('.project').fadeIn(800);
       $('#contact').fadeIn(1000);
